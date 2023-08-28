a8a29f06015139af61e8b7fbcca9dda1bd90d5c3
EshuShango <tobiakinyemi21@gmail.com>
2023-08-28 04:00:07 -0500

    quick upd, added empty dir in organized manner

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-import "../css/style.css";
-import { setupCounter } from "./counter";
-
-// console.log(import.meta.env);
-
-setupCounter(document.querySelector("#counter"));
---
+++ src/vite.config.js
@@ -0,0 +1,31 @@
+/// <reference types="vitest" />
+import { configDefaults } from "vitest/config";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  //* vitest config
+  test: {
+    exclude: [...configDefaults.exclude, "packages/template/*"],
+    include: ["**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+  },
+  //* vite config
+  clearScreen: false,
+  envPrefix: "APP_",
+  // Choose which message to display on startup.
+  logLevel: "info",
+  envDir: "env",
+  server: {
+    // opens the browser automatically
+    open: "index.html",
+    port: 3000,
+    // if you want to restrict the server to a specific ip address
+    strictPort: true,
+  },
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  css: {
+    devSourcemap: true,
+  },
+});
